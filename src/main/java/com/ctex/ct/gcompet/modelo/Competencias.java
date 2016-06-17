4628f84cfba573d716bd21a5a46b45ffc1ac170c
ansuattigui <ansuattigui@gmail.com>
2016-06-17 12:32:01 -0300

    Ajustes gerais para entrada em produção

---
+++ src/main/java/com/ctex/ct/gcompet/modelo/Competencias.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ctex.ct.gcompet.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author ralfh
+ */
+@Entity
+@Table(name = "competencias")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Competencias.findAll", query = "SELECT c FROM Competencias c"),
+    @NamedQuery(name = "Competencias.findById", query = "SELECT c FROM Competencias c WHERE c.id = :id"),
+    @NamedQuery(name = "Competencias.findByNome", query = "SELECT c FROM Competencias c WHERE c.nome = :nome")})
+public class Competencias implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 255)
+    @Column(name = "nome")
+    private String nome;
+
+    public Competencias() {
+    }
+
+    public Competencias(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Competencias)) {
+            return false;
+        }
+        Competencias other = (Competencias) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.ctex.ct.gcompet.modelo.Competencias[ id=" + id + " ]";
+    }
+    
+}
