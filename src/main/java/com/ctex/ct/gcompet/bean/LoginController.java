4a0d57111aabac609b6c5dff5937a069d6235646
ansuattigui <ansuattigui@gmail.com>
2016-07-13 15:54:55 -0300

    Finalização do protótipo para vinculação de dados

--- src/main/java/com/ctex/ct/gcompet/bean/LoginController.java
+++ src/main/java/com/ctex/ct/gcompet/bean/LoginController.java
@@ -8,7 +8,7 @@ package com.ctex.ct.gcompet.bean;
 import com.ctex.ct.gcompet.modelo.Usuarios;
 import java.io.Serializable;
 import javax.ejb.EJB;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
@@ -18,7 +18,7 @@ import javax.inject.Named;
  * @author Ralfh
  */
 @Named("loginController")
-@RequestScoped
+@SessionScoped
 public class LoginController implements Serializable {
     
     @EJB
